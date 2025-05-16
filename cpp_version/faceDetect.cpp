fd3492ad396f41a999247310e8339bb6896efba3
mishamickevicius <vmmgame84@gmail.com>
2025-05-16 09:46:59 -0500

    changes

---
+++ cpp_version/faceDetect.cpp
@@ -0,0 +1,11 @@
+#include <opencv2/objdetect.hpp>
+#include <opencv2/core/mat.hpp>
+
+/*
+isFaceDetected() is a function that will take an image as an input and 
+will output a 1(true) if a human face is detected 
+and output 0(false) if no face is detected 
+*/
+bool isFaceDetected() {
+    cv::InputArray
+}
---
+++ cpp_version/faceDetect.hpp
@@ -0,0 +1,8 @@
+#ifndef FACEDETECT_HPP
+#define FACEDETECT_HPP
+
+#include <opencv2/core/mat.hpp>
+
+int isFaceDetected();
+
+#endif
